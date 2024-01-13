8328a752d3802b7714a8f76b87ae7329a65e7b98
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-13 16:32:58 -0300

    feat: adicionando o button, card e header components

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button } from "./button";
+import { Card } from "./card";
+
+const Header = () => {
+  return (
+    <Card className="flex items-center justify-between p-[1.875rem]">
+      <Link href="/">
+        <h1 className=" text-lg font-semibold">
+          <span className="text-primary">EAD</span> Ensino
+        </h1>
+      </Link>
+
+      <div className="flex items-center">
+        <Link href="/">
+          <h1 className=" mr-5 text-sm font-semibold">Entrar</h1>
+        </Link>
+        <Link href="/">
+          <h1 className=" text-sm font-semibold">Cadastrar</h1>
+        </Link>
+      </div>
+    </Card>
+  );
+};
+
+export default Header;
