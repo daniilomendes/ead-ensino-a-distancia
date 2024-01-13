dedf0883e4b0bc9fea74eca842141f4850a24b90
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-13 16:49:11 -0300

    chore: adicionando a imagem de background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="container mx-auto bg-presentation-background bg-cover bg-no-repeat p-5 h-auto">
+
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,11 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -17,6 +16,9 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "presentation-background": "url(/backgroundPresentationSection.png)"
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -59,12 +61,12 @@ module.exports = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
@@ -74,4 +76,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
