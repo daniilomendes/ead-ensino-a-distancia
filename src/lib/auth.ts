f63510f138d6978bd0867c80541e8bcdd64b6b61
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-15 22:21:26 -0300

    chore: adicionando o CredentialsProvider para autenticação com email e senha

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 import { prismaClient } from "./prisma";
 import { Adapter } from "next-auth/adapters";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prismaClient) as Adapter,
@@ -10,6 +11,30 @@ export const authOptions: AuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      credentials: {
+        email: { label: "email", type: "text", placeholder: "jsmith" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "1",
+          name: "J Smith",
+          email: "jsmith@example.com",
+          password: "secret123",
+        };
+
+        if (
+          user &&
+          user?.email === credentials?.email &&
+          user?.password === credentials?.password
+        ) {
+          return user;
+        }
+
+        return null;
+      },
     }),
   ],
   callbacks: {
